b6e95294bfe8e2aabfdd87ef24866363936acfe2
naeczennie <potatoava321@gmail.com>
2025-02-23 22:07:34 -0500

    Homework 2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+
+    private float _xspeed;
+    private float _yspeed;
+
+    public float speed = 3;
+
+    private string ImputX = "Horizontal";
+    private string ImputY = "Vertical";
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xspeed = Input.GetAxis(ImputX);
+        _yspeed = Input.GetAxis(ImputY);
+
+        _rigidbody2D.velocity = new Vector2(_xspeed, _yspeed) * speed;
+    }
 
 }
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -2,5 +2,52 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
+
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
+
+    private void Update()
+    {
+        TimerMethod();
+        Shoot();
+    }
+
+    private void TimerMethod()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime < 0)
+            {
+                _canShoot = true;
+                _currentTime = Timer;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1) && _canShoot)
+        {
+            GameObject bullet2 = Instantiate(preFab, bulletSpawn.position, Quaternion.identity);
+
+            bullet2.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
+        }
+    }
 
 }
